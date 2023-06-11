40bbcb8d4b9f558bf5c8427b270009c1deba1675
iljapapilloud <131356467+iljapapilloud@users.noreply.github.com>
2023-06-11 10:31:01 +0200

    Fitoride eng
    Translate app into english

--- fitoride-app.py
+++ fitoride-app.py
@@ -9,59 +9,59 @@ st.set_page_config(layout="wide") # um die Webseite breiter zu machen
 
 st.markdown("<h1 style='text-align: center; color: YellowGreen;'>Fitoride</h1>", unsafe_allow_html=True) # Jetzt Haupttitle zentriert
 
-st.markdown("Bike-Fitting statique pour hommes et femmes à partir de 15 ans. Vous avez besoin d'un centimètre, d'un livre, de quelques clés Allen - inbus")
+st.markdown("Static Bike-Fitting unisex 15-75 years old. You nead a measuring tape, a book, and some Allen keys")
 
-button = st.sidebar.radio("**Procédez au Bike-Fitting étape par étape en commançant par l'étape 1. A chaque étape, procédez aux réglages voulus avant de passer à l'étape suivante**", ('Etape 1', 'Etape 2', 'Etape 3'))
+button = st.sidebar.radio("**Do the Bike-Fitting step by step, from first step onwards. At each step, apply the result before going to the next step**", ('Etape 1', 'Etape 2', 'Etape 3'))
 #button2 = st.sidebar.radio("**PF-I Gesamt**", ('Topics', 'Trends'))
 
 if button == 'Etape 1':
-    st.markdown("<h4 style='text-align: left; color: YellowGreen;'>Hauteur de selle</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: left; color: YellowGreen;'>Saddle height</h4>", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
 
     with col1:
-        st.markdown("Pieds nus avec votre tenue de sport, mettre un livre entre vos jambes et se mettre contre un mur. Le livre doit toucher, non pas presser, le pelvis.")
-        st.markdown("A l'aide d'un crayon, faire un trait contre le mur à la hauteur de la tranche supérieure du livre.")
-        innennaht = st.number_input(label = "Reporter la distance du sol au trait en centimètres et millimètres (p.ex. 83,4): ")
-        st.markdown("A l'aide d'une app de mesure d'angles, mesurer l'angle entre le tube de selle et une droite imaginaire.")
-        winkelsattelr = st.number_input(label = "Reporter l'angle en degré (p.ex. 19,4): ")
+        st.markdown("Barefoot with your bicycle outfit on you, face against a wall, put the book between your legs touching, not pressing, the pelvis.")
+        st.markdown("With a pencile, mark on the wall where the top edge of the book touch the wall.")
+        innennaht = st.number_input(label = "Input the distance (in cm) from the ground to the mark on the wall here (f.ex. 83.4): ")
+        st.markdown("With a app, notice the angle between your saddle tube and an imaginary vertical straight line.")
+        winkelsattelr = st.number_input(label = "Report this angle here (p.ex. 19.4): ")
         sattelhoehe = 7.41 + (0.82*float(innennaht)) - 0.1*28 + 0.003*(float(innennaht)*float(winkelsattelr))
         sattelhoehemax = 7.41 + (0.82*float(innennaht)) - 0.1*26 + 0.003*(float(innennaht)*float(winkelsattelr))
         sattelhoehemin = 7.41 + (0.82*float(innennaht)) - 0.1*30 + 0.003*(float(innennaht)*float(winkelsattelr))
-        st.write("\nVotre hauteur de selle optimale en cm est: ", sattelhoehe)
-        st.write("\nVotre hauteur de selle en cm ne doit pas être inférieure à:  ", sattelhoehemin)
-        st.write("\nVotre hauteur de selle en cm ne doit pas être supérieure à:  ", sattelhoehemax)
+        st.write("\nYour optimal saddle height in cm is: ", sattelhoehe)
+        st.write("\nYour saddle height should not be under:  ", sattelhoehemin)
+        st.write("\nYour saddle height should not exceed:  ", sattelhoehemax)
 
     with col2:
-        st.image("Beinc.png",width=150, caption='Longeur interne de la jambe')
-        st.image("bike3ac.png",width=150, caption='Angle tube de selle/droite')
-        st.image("bike2ac.png",width=150, caption='Hauteur de selle axe pédalier/sommet selle')
+        st.image("Beinc.png",width=150, caption='Inseam length')
+        st.image("bike3ac.png",width=150, caption='Angle saddle tube/imaginary straight line')
+        st.image("bike2ac.png",width=150, caption='Saddle height from middle of the chainring/bottom of saddle')
 
 if button == 'Etape 2':
-    st.markdown("<h4 style='text-align: left; color: YellowGreen;'>Selle en avant/en arrière</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: left; color: YellowGreen;'>Saddle forth/back</h4>", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
 
     with col1:
-        sattellang = st.number_input("Longueur de la selle en centimètres et millimètres (p.ex. 27.5): ")
+        sattellang = st.number_input("Report the lenght of your saddle in cm (p.ex. 27.5): ")
         sattelmittep = float(sattellang)/2
         sattelgravit = (float(sattellang)*2)/3
         sattelgp = sattelgravit - sattelmittep
-        st.markdown("Placer la selle de sorte que le centre de la selle (Z) soit au centre du tube de selle.")
-        st.write("Vous pouvez avancer la selle jusqu'au point (G), en cm au maximum de ", sattelgp)
-        st.markdown("Votre selle doit être parallèle au sol, ne pas pencher vers le bas ou monter vers le haut.")
+        st.markdown("Move the saddle until its middle point (Z) is at the middle point of the saddle tube.")
+        st.write("You can move the saddle forward until (G) ", sattelgp)
+        st.markdown("You saddle must be parallel to the ground. It should not point up or down.")
 
 
     with col2:
-        st.image("Sattelc.png",width=200, caption='Selle en avant/en arrière, centre(Z), gravité(G)')
+        st.image("Sattelc.png",width=200, caption='Saddle forth/back, centre(Z), gravity(G)')
 
 if button == 'Etape 3':
-    st.markdown("<h4 style='text-align: left; color: YellowGreen;'>Haut du corps</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: left; color: YellowGreen;'>Torso and arms</h4>", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
 
     with col1:
-        arm = st.number_input("Longueur de votre bras du poignet (jonction main-poignet) à la base de l'épaule (jonction épaule-torse) en centimètres: ")
+        arm = st.number_input("Arm length from your wrist (jonction hand-wrist) to shoulder (jonction shoulder-torso) in cm: ")
         armcorrup = (float(arm)*0.04)
         armcorrdown = (float(arm)*0.08)
         armup = float(arm) - float(armcorrup)
@@ -75,18 +75,18 @@ if button == 'Etape 3':
         armlenkersmin = (float(armdown)/math.sin(math.radians(40)))*math.sin(math.radians(100))
         armlenkersmax = (float(armdown)/math.sin(math.radians(50)))*math.sin(math.radians(110))
 
-        st.markdown("Si vous préférez rouler avec les bras plus tendus, alors...")
+        st.markdown("If you want to drive with outstreched arms, then...")
 
-        st.write("... la distance optimale du centre de la selle au centre du guidon en cm est de:", armlenkeropt)
-        st.write("... du centre de la selle au centre du guidon, cette distance en cm ne devrait pas être supérieure à:", armlenkermin)
-        st.write("... du centre de la selle au centre du guidon, cette distance en cm ne devrait pas être inférieure à:", armlenkermax)
+        st.write("... the optimal distance from the middle of the saddle to the middle of the handlebars is (in cm):", armlenkeropt)
+        st.write("... the distance from the middle of the saddle to the middle of the handlebars (in cm) should not exceed:", armlenkermin)
+        st.write("... the distance from the middle of the saddle to the middle of the handlebars (in cm) should not be less than:", armlenkermax)
 
 
-        st.markdown("Si vous préférez rouler avec les bras moins tendus, alors...")
+        st.markdown("If you prefer driving with arms less outstreched, then...")
 
-        st.write("... la distance optimale du centre de la selle au centre du guidon en cm est de:", armlenkersopt)
-        st.write("... du centre de la selle au centre du guidon, cette distance en cm ne devrait pas être supérieure à:", armlenkersmin)
-        st.write("... du centre de la selle au centre du guidon, cette distance en cm ne devrait pas être inférieure à:", armlenkersmax)
+        st.write("... the optimal distance from the middle of the saddle to the middle of the handlebars is (in cm):", armlenkersopt)
+        st.write("... the distance from the middle of the saddle to the middle of the handlebars (in cm) should not exceed:", armlenkersmin)
+        st.write("... the distance from the middle of the saddle to the middle of the handlebars (in cm) should not be less than:", armlenkersmax)
 
     with col2:
-        st.image("armc.png",width=200,caption='Longueur du bras')
+        st.image("armc.png",width=200,caption='Arm length')
